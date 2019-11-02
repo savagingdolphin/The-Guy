ea7ca1dec3ff95fcf228f37ee7d474d3f91f2273
savagingdolphin <57302365+savagingdolphin@users.noreply.github.com>
2019-11-02 21:38:01 +0000

    First Commit
    First Commit

---
+++ Assets/Scripts/Guy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guy : MonoBehaviour
+{
+    [SerializeField] string myName = "Mikey";
+    [SerializeField] ParticleSystem explosion = default;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        Debug.Log("Hello " + myName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        audioSource.Play();
+        explosion.Play();
+    }
+
+}
